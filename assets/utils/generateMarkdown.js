e75e53a3170ef32712302af6d44e30f38e2d5466
Brady Mavetz <b.mavetz@outlook.com>
2022-04-14 19:38:18 -0500

    added url for license options

--- assets/utils/generateMarkdown.js
+++ assets/utils/generateMarkdown.js
@@ -23,16 +23,26 @@ function renderLicenseLink(license) {
 if(license == 'none'){
   return('');
 }else if(license == 'MIT'){
-  return('https://img.shields.io/badge/license-MIT-yellowgreen');
+  return('https://opensource.org/licenses/MIT');
 }else if(license == 'Apache'){
   return('https://opensource.org/licenses/Apache-2.0');
 }else if(license == 'GPL'){
-  return('https://img.shields.io/badge/License-GPL%20v3-red.svg');
+  return('https://www.gnu.org/licenses/gpl-3.0');
 }
 }
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(license == 'none'){
+    return('');
+  }else if(license == 'MIT'){
+    return('https://opensource.org/licenses/MIT');
+  }else if(license == 'Apache'){
+    return('https://opensource.org/licenses/Apache-2.0');
+  }else if(license == 'GPL'){
+    return('https://www.gnu.org/licenses/gpl-3.0');
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
